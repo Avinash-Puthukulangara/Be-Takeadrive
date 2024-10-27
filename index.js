5fd8f18265a9c5449ae2eacfb3fc69107d4b6a1f
Avinash P <aviputhukulangara@gmail.com>
2024-10-27 18:12:12 +0530

    index eeditd

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = 3003;
 connectDb();
 
 app.use(cors({
-    origin:["http://localhost:5173"],
+    origin:["http://localhost:5173","https://uclient-takeadrive.vercel.app"],
     credentials:true,
     methods:['GET', 'POST', 'PUT', 'DELETE']
 }));
