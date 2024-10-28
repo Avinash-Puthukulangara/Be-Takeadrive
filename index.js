50bf5a863ade079790675133070e4d66d74b7f6f
Avinash P <aviputhukulangara@gmail.com>
2024-10-28 16:18:56 +0530

    edis

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors({
         "http://localhost:5173",
         "https://uiclient-takeadrive-9w6ms5j5z-avinash-ps-projects.vercel.app",
         "https://u-takeadrive.vercel.app",
-        "https://takeadrive-user.netlify.app"
+        "https://takeadrive-u.netlify.app"
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
