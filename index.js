56915358447e984c3b7a1e30063cd184474f3146
Avinash P <aviputhukulangara@gmail.com>
2024-10-28 10:33:44 +0530

    edis

--- index.js
+++ index.js
@@ -2,31 +2,32 @@ import express from 'express';
 import { connectDb } from './config/db.js';
 import { apiRouter } from './routes/index.js';
 import cookieParser from 'cookie-parser';
-import cors from 'cors'
-const app = new express();
+import cors from 'cors';
+
+const app = express();
 const PORT = 3003;
 
 connectDb();
 
 app.use(cors({
-    origin:"*",
-    credentials:true,
-    methods:['GET', 'POST', 'PUT', 'DELETE']
+    origin: [
+        "http://localhost:5173",
+        "https://uiclient-takeadrive-9w6ms5j5z-avinash-ps-projects.vercel.app",
+        "https://u-takeadrive.vercel.app"
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
+
 app.use(cookieParser());
 app.use(express.json());
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send('Hello World!');
-})
-
+});
 
 app.use('/api', apiRouter);
 
-
-
-
-
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
-})
+});
